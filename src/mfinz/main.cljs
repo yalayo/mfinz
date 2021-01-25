ae987d7a4d130b2e6295849e835943cd2a50713c
Mylab <labs@onmycrowd.com>
2021-01-24 17:55:52 -0700

    Add needed js dependencies.

--- src/mfinz/main.cljs
+++ src/mfinz/main.cljs
@@ -1,2 +1,10 @@
-(ns mfinz.main)
+(ns mfinz.main
+  (:require [reagent.core :as r]
+            [re-frame.core :as re-frame]))
 
+(defn app []
+  [:p "Under construction"])
+
+(defn figwheel-rn-root []
+  (re-frame/dispatch-sync [:initialize-app])
+  (r/as-element [app]))
